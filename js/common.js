12dc916d099a55059dabf2e54dcc4f30844d011d
Sergeyprostoas <Sergeyprostoas@gmail.com>
2018-11-13 22:25:04 +0300

    Настройка медиа

--- js/common.js
+++ js/common.js
@@ -1,18 +1,19 @@
-$(document).ready(function() {
-	var owlHeader = $('.slider');
-	owlHeader.owlCarousel({
-		loop: true,
-		autoplay: true,
-		slideSpeed: 300,
-		paginationSpeed: 400,
-		autoplayHoverPause: true,
-		animateIn: 'fadeIn',
-		animateOut: 'fadeOut',
-		nav: true,
-		navText : ["<img src='img/slider_arrow_l.png'>","<img src='img/slider_arrow_r.png'>"],
-		dots: true,
-		items: 1
-	});
+$(".nav--search-show").click(function(){
+	$(".nav--search-block").show();
+});
+
+$(".nav--search-hide").click(function(){
+	$(".nav--search-block").hide();
+});
+
+$('.slider').owlCarousel({
+    loop:true,
+    margin:30,
+    autoplay: true,
+	slideSpeed: 300,
+	paginationSpeed: 400,
+	autoplayHoverPause: true,
+    items: 1
 })
 
 $('.block-slider').owlCarousel({
@@ -23,12 +24,12 @@ $('.block-slider').owlCarousel({
 	paginationSpeed: 400,
     responsive:{
         0:{
-            items:1
+            items:2
         },
-        600:{
-            items:3
+        1200:{
+            items:5
         },
-        1000:{
+        1600:{
             items:3
         }
     }
