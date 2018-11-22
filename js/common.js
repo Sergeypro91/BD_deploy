587de5efe6f08e2796804a4b53e620df457c559c
Sergeyprostoas <Sergeyprostoas@gmail.com>
2018-11-22 21:32:27 +0300

    Редакция медиа

--- js/common.js
+++ js/common.js
@@ -113,3 +113,42 @@ function pad(n) {
   return (n < 10 ? '0' : '') + n;
 }
 
+//Heder scroll efect
+// Hide Header on on scroll down
+var didScroll;
+var lastScrollTop = 0;
+var delta = 5;
+var navbarHeight = $('header').outerHeight();
+
+$(window).scroll(function(event){
+    didScroll = true;
+});
+
+setInterval(function() {
+    if (didScroll) {
+        hasScrolled();
+        didScroll = false;
+    }
+}, 250);
+
+function hasScrolled() {
+    var st = $(this).scrollTop();
+    
+    // Make sure they scroll more than delta
+    if(Math.abs(lastScrollTop - st) <= delta)
+        return;
+    
+    // If they scrolled down and are past the navbar, add class .header-up.
+    // This is necessary so you never see what is "behind" the navbar.
+    if (st > lastScrollTop && st > navbarHeight){
+        // Scroll Down
+        $('header').removeClass('header').addClass('header-up');
+    } else {
+        // Scroll Up
+        if(st + $(window).height() < $(document).height()) {
+            $('header').removeClass('header-up').addClass('header');
+        }
+    }
+    
+    lastScrollTop = st;
+}
